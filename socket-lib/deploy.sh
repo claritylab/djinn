7d7a20f96c9a29acbe2c22565c4ac68e7d72a0e9
Yiping Kang <ypkang@umich.edu>
2014-10-29 00:24:55 -0400

    add deploy.sh

--- socket-lib/deploy.sh
+++ socket-lib/deploy.sh
@@ -1,8 +1,9 @@
-DIR=/home/jahausw/projects/deep-ipa
-KALDI_DIR=/home/jahausw/projects/dnn/kaldi-trunk/src/nnetbin
+DIR=/home/ypkang/git/deep-ipa
+KALDI_DIR=/home/ypkang/kaldi-trunk/src/nnetbin
 
 cp libsocket.a $DIR
 cp socket.h $DIR/img/src
 cp socket.h $DIR/dnn/src
 cp socket.h $DIR/nlp/src
 cp libsocket.a $KALDI_DIR 
+cp socket.h $KALDI_DIR
