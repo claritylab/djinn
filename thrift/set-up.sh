532731a818e4053e06c06bacfcbd3acdd3f59bed
jhauswald <jahausw@umich.edu>
2014-11-12 05:38:50 -0500

    tweak

--- thrift/set-up.sh
+++ thrift/set-up.sh
@@ -1,45 +1,51 @@
 #!/bin/bash
 
-# time to sleep between processe
+# time to sleep between processes
 t=10;
 stats_chill=30
 # dnn server params
-for i in 2 4 6 8
+for gpu in 1 2 4 8
 do
-numservers=$i
-serverid=0
+  for rps in 10 25 50
+  do
+    numservers=$gpu
+    serverid=0
 
-# client params
-numclients=$numservers
-clientid=1
+    # client params
+    numclients=$numservers
+    clientid=1
 
-# runner params
-numrunners=$numclients
-rps=25
-# in seconds
-runtime=120
+    # runner params
+    numrunners=$numclients
+    rps=$rps
+    # in seconds
+    runtime=1200
 
-echo "Setting up server and clients..."
-cd server > /dev/null;
-./servers.sh $numservers $serverid
-cd - && cd client > /dev/null;
-./clients.sh $numclients $clientid
-cd - > /dev/null;
+    echo "Setting up server and clients..."
+    cd server > /dev/null;
+    ./servers.sh $numservers $serverid
+    cd - && cd client > /dev/null;
+    ./clients.sh $numclients $clientid
+    cd - > /dev/null;
 
-sleep $t;
-echo "Set up done."
+    sleep $t;
+    echo "Set up done."
 
-# start collecting stats
-ulog=gpu-stats_util_${numservers}_${numrunners}_${rps}.txt
-nvidia-smi stats -d gpuUtil -i $serverid --filename $ulog &
-pid_stats=$!
+    # start collecting stats
+    ulog=gpu-stats_util_${numservers}_${numrunners}_${rps}_${runtime}.txt
+    nvidia-smi stats -d gpuUtil -i $serverid --filename $ulog &
+    pid_stats=$!
 
-# chill...
-sleep $stats_chill;
+    # chill...
+    sleep $stats_chill;
 
-echo "Let's run."
-./runners.sh $numrunners $rps $runtime
+    echo "Let's run."
+    ./runners.sh $numrunners $rps $runtime
 
-sleep $stats_chill;
-kill $pid_stats
+    sleep $runtime;
+    kill $pid_stats
+  done
 done
+
+pkill Dnn*
+pkill treadmill
