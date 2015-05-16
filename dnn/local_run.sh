633a6318982616031184afbed63601ccbc01d7c1
Yiping Kang <ypkang@umich.edu>
2015-05-16 14:00:57 -0400

    script tw

--- dnn/local_run.sh
+++ dnn/local_run.sh
@@ -6,8 +6,7 @@ include_transfer=$3
 csv=$4
 input=input/${network}.in
 trial=$5
-layer_csv=$6
 
 export OPENBLAS_NUM_THREADS=4
 
-./dnn-server --gpu $gpu --transfer $include_transfer --csv $csv --trial $trial --network $network --input $input --layer_csv $layer_csv
+./dnn-server --gpu $gpu --transfer $include_transfer --csv $csv --trial $trial --network $network --input $input
