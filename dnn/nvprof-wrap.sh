ec88a3d6c99bb5c21bd58a50840ceb58b07f4914
Yiping Kang <ypkang@umich.edu>
2015-05-19 13:30:30 -0400

    unify fc layers name

--- dnn/nvprof-wrap.sh
+++ dnn/nvprof-wrap.sh
@@ -4,19 +4,19 @@ input=$3
 trial=1
 gpu=1
 
-for k in imc
+for k in VGG
 do
     rm -rf $k/*
-    ./nvprof-metrics.sh $k \
-                        input/$k.in \
-                        $trial \
-                        $gpu
-
-    ./nvprof-timing.sh $k \
-                       input/$k.in \
-                       $trial \
-                       $gpu
-
+#    ./nvprof-metrics.sh $k \
+#                        input/$k.in \
+#                        $trial \
+#                        $gpu
+#
+#    ./nvprof-timing.sh $k \
+#                       input/$k.in \
+#                       $trial \
+#                       $gpu
+#
     ./nvprof-trace.sh $k \
                       input/$k.in \
                       $trial \
